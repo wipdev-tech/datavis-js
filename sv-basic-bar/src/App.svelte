71756c26ab8b14199153c532272e42138448b0d2
waseem <waseemmedhatg@gmail.com>
2022-04-06 12:32:44 +0200

    sv-basic-bar: barebones chart

--- sv-basic-bar/src/App.svelte
+++ sv-basic-bar/src/App.svelte
@@ -1,30 +1,48 @@
 <script>
-	export let name;
+  import { scaleLinear, scaleBand } from "d3-scale";
+  import AxisTop from "./components/AxisTop.svelte";
+
+  // data
+  const data = [
+    { cat: "Placebo", val: 0.35126 },
+    { cat: "Low Dose", val: 0.68901 },
+    { cat: "High Dose", val: 0.62023 },
+  ];
+
+  // width and height
+  let width = 960;
+  let height = 300;
+
+  const margin = { top: 25, right: 10, bottom: 10, left: 10 };
+
+  let innerWidth = width - margin.left - margin.right;
+  let innerHeight = height - margin.top - margin.bottom;
+
+  // scales
+  const xScale = scaleLinear().domain([0, 1]).range([0, innerWidth]);
+  const yScale = scaleBand()
+    .domain(data.map((d) => d.cat))
+    .range([0, innerHeight])
+    .padding(0.3);
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<svg {width} {height}>
+  <g transform="translate({margin.left}, {margin.top})">
+    <AxisTop {xScale} {innerWidth} />
+    {#each data as { cat, val }}
+      <rect
+        class="bg"
+        y={yScale(cat) - 8}
+        width={innerWidth}
+        height={yScale.bandwidth() + 16}
+      />
+      <rect y={yScale(cat)} width={xScale(val)} height={yScale.bandwidth()} />
+    {/each}
+  </g>
+</svg>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .bg {
+    fill: lightgray;
+  }
 </style>
---
+++ sv-basic-bar/src/components/AxisTop.svelte
@@ -0,0 +1,18 @@
+<script>
+  export let xScale;
+  export let innerWidth;
+</script>
+
+<g>
+  <line x2={innerWidth} stroke="black" />
+  {#each xScale.ticks() as tick}
+    <text x={xScale(tick)} y="-10">{tick}</text>
+    <line transform="translate({xScale(tick)}, 0)" y1="-4" stroke="black" />
+  {/each}
+</g>
+
+<style>
+  text {
+    text-anchor: middle;
+  }
+</style>
